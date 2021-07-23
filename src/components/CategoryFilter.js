0fdcb4894602ac8b78e41a899cd359a626e2edd3
thneves <tsneves11@gmail.com>
2021-07-23 14:11:01 -0300

    Implement Header CSS style

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,17 +1,26 @@
 import PropTypes from 'prop-types';
+import ProfileIcon from '../images/profile-icon.png';
+import '../styles/CategoryFilter.css';
 
 const CategoryFilter = ({ handleFilter }) => {
   const categories = ['All', 'Thriller', 'Biography', 'Mistery', 'Sci-Fi', 'Drama', 'Learning'];
 
   return (
-    <>
-      <label htmlFor="books-categories">
-        Filter by Category:
-        <select id="books-categories" name="categories" onChange={handleFilter}>
-          { categories.map((cat) => <option key={cat} value={cat}>{cat}</option>)}
-        </select>
-      </label>
-    </>
+    <header id="top-panel">
+      <nav className="nav">
+        <h1 className="bookstore-h1">Bookstore - CMS</h1>
+        <h4 className="books-h4">BOOKS</h4>
+        <label htmlFor="books-categories" className="books-categories">
+          CATEGORIES&nbsp;&nbsp;
+          <select id="books-categories" name="categories" onChange={handleFilter}>
+            { categories.map((cat) => <option key={cat} value={cat}>{cat}</option>)}
+          </select>
+        </label>
+      </nav>
+      <figure className="profile-circle">
+        <img className="profile-icon" src={ProfileIcon} alt="user icon" />
+      </figure>
+    </header>
   );
 };
 
